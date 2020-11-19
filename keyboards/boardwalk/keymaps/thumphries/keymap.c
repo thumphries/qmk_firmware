2f1a1603166b345601dfcf7d0b6ee16b8e0097ac
Tim Humphries <tim@utf8.me>
2020-11-18 21:24:41 -0800

    Flip eql/mins, wipe eeprom on start

--- keyboards/boardwalk/keymaps/thumphries/keymap.c
+++ keyboards/boardwalk/keymaps/thumphries/keymap.c
@@ -24,7 +24,7 @@ enum layer {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_hhkb(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    M(4),    M(3),    KC_H,    KC_J,    KC_K,    KC_L,    LT(_FN, KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    M(4),    M(3),    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_MPLY,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_BSLS, _______,
+        _______, RGB_TOG, _______, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_BSLS, _______,
                  _______, _______, _______, _______,          _______, _______,          TG(_FN), _______, _______, RESET
     )
 };
@@ -92,6 +92,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 // RGB stuff
 void matrix_init_user(void) {
+    eeconfig_init();
     rgblight_enable();
     rgblight_setrgb(0x00,  0xFF, 0xFF);
 };
