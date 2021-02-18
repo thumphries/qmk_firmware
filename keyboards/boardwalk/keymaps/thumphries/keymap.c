4207028df51a5af254c7b68dc066e4bd9e4e7da4
Tim Humphries <tim@utf8.me>
2021-02-18 01:25:59 -0800

    mod4 workaround

--- keyboards/boardwalk/keymaps/thumphries/keymap.c
+++ keyboards/boardwalk/keymaps/thumphries/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_hhkb(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    M(4),    M(3),    KC_H,    KC_J,    KC_K,    KC_L,    LT(_FN, KC_SCLN), KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    M(4),    M(3),    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    M(4),    KC_LGUI, KC_H,    KC_J,    KC_K,    KC_L,    LT(_FN, KC_SCLN), KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    M(4),    KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                 MO(_NAV), MO(_FN), _______, _______,          _______,  KC_SPC,          KC_LGUI, KC_ENT, KC_LALT, TG(_NAV)
     ),
 
